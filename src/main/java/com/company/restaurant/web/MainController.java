6ad3cb4f8497c1f79635867d4832db7886a8263b
khomyakeugene <khomyak_eugene@yahoo.com>
2016-08-05 09:48:06 +0300

    JEE Unit 9 Homework

--- src/main/java/com/company/restaurant/web/MainController.java
+++ src/main/java/com/company/restaurant/web/MainController.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Date;
-import java.util.Map;
-
 /**
  * Created by Yevhen on 28.07.2016.
  */
 @Controller
 public class MainController {
+    private static final String START_VIEW_NAME = "index";
+    private static final String RESTAURANT_NAME_VAR_NAME = "restaurantName";
+    private static final String RESTAURANT_ADDRESS_VAR_NAME = "restaurantAddress";
+    private static final String RESTAURANT_E_MAIL_VAR_NAME = "restaurantEMail";
+    private static final String RESTAURANT_EMBLEM_IMAGE_VAR_NAME = "restaurantEmblemImage";
+    private static final String RESTAURANT_TRANSPORT_IMAGE_MAP_VAR_NAME = "restaurantTransportMapImage";
+    private static final String RESTAURANT_RESTAURANT_SCHEMA_IMAGE_MAP_VAR_NAME = "restaurantRestaurantSchema";
+
     private CommonDataService commonDataService;
 
     @Autowired
@@ -25,9 +30,16 @@ public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("restaurantName", commonDataService.getRestaurantName());
 
-        modelAndView.setViewName("index");
+        modelAndView.addObject(RESTAURANT_NAME_VAR_NAME, commonDataService.getRestaurantName());
+        modelAndView.addObject(RESTAURANT_ADDRESS_VAR_NAME, commonDataService.getRestaurantAddress());
+        modelAndView.addObject(RESTAURANT_E_MAIL_VAR_NAME, commonDataService.getRestaurantEMail());
+        modelAndView.addObject(RESTAURANT_EMBLEM_IMAGE_VAR_NAME, commonDataService.getEmblemImage());
+        modelAndView.addObject(RESTAURANT_TRANSPORT_IMAGE_MAP_VAR_NAME, commonDataService.getTransportMapImage());
+        modelAndView.addObject(RESTAURANT_RESTAURANT_SCHEMA_IMAGE_MAP_VAR_NAME,
+                commonDataService.getRestaurantSchemeImage());
+
+        modelAndView.setViewName(START_VIEW_NAME);
 
         return modelAndView;
     }
